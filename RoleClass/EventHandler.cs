9d2f00c286efb2876b1bf05bd5c723c23df742d6
lordofkhaos <30538145+lordofkhaos@users.noreply.github.com>
2018-08-17 12:50:28 -0400

    Add files via upload

---
+++ RoleClass/EventHandler.cs
@@ -0,0 +1,47 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.EventSystem.Events;
+using System;
+using System.Collections.Generic;
+
+namespace Smod.TestPlugin
+{
+    class RoundEventHandler : IEventHandlerPlayerJoin
+    {
+		private Plugin plugin;
+
+		public RoundEventHandler(Plugin plugin)
+		{
+			this.plugin = plugin;
+		}
+
+        public void OnPlayerJoin(PlayerJoinEvent ev) {
+            if (ev.Player.SteamId == "76561198071607345" && ev.Player.GetUserGroup().Name == string.Empty)
+                ev.Player.SetRank("aqua", "PLUGIN DEV");
+        }
+
+        public void OnPlayerSpawn(PlayerSpawnEvent ev) {
+            string rank = ev.Player.GetRankName();
+            Dictionary<string, string> dictionary = plugin.GetConfigDict("k_roleclass");
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (KeyValuePair<string, string> x in dictionary)
+            {
+                var j = Convert.ToInt32(x.Value);
+                dict.Add(x.Key, j);
+            }
+            foreach (KeyValuePair<string, int> m in dict) 
+            {
+                if (rank != null) 
+                {
+                    if (m.Key == rank)
+                    {
+                        var itemType = (ItemType)m.Value;
+                        ev.Player.GiveItem(itemType);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RoleClass/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using Smod.TestPlugin;
+using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Collections.Generic;
+
+namespace ExamplePlugin
+{
+	[PluginDetails(
+		author = "lordofkhaos",
+		name = "Test",
+		description = "Test Plugin",
+		id = "lordofkhaos.test.plugin",
+		version = "3.0",
+		SmodMajor = 3,
+		SmodMinor = 1,
+		SmodRevision = 12
+		)]
+	class ExamplePlugin : Plugin
+	{
+		public override void OnDisable()
+        {
+            this.Info("Plugin disabled!");
+        }
+
+		public override void OnEnable()
+		{
+			this.Info("Test Plugin has loaded :)");
+			this.Info("Config value: " + this.GetConfigString("test"));
+		}
+
+		public override void Register()
+		{
+			// Register Events
+			this.AddEventHandlers(new RoundEventHandler(this));
+			// Register Commands
+			this.AddCommand("hello", new HelloWorldCommand(this));
+			// Register config settings
+			this.AddConfig(new Smod2.Config.ConfigSetting("test", "yes", Smod2.Config.SettingType.STRING, true, "test"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("k_roleclass", new Dictionary<string, string>(), true, Smod2.Config.SettingType.DICTIONARY, true, "Roles and items"));
+		}
+	}
+}
