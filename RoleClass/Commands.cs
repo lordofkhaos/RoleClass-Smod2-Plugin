3bbb46b5832a4bd9bc864284c27b9a1bcaca2162
lordofkhaos <30538145+lordofkhaos@users.noreply.github.com>
2018-08-20 09:49:43 -0400

    Add files via upload

--- RoleClass/Commands.cs
+++ RoleClass/Commands.cs
@@ -1,4 +1,5 @@
 ﻿using Smod2.Commands;
+using Smod2;
 //using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -33,56 +34,76 @@ namespace ExamplePlugin
         //    return new string[] { "Currently all this command does is send this string!" };
         //}
 
+
         public string[] OnCall(ICommandSender sender, string[] args)
         {
+            Smod2.API.Player a = (Smod2.API.Player)sender;
+            var s64 = a.SteamId;
+            string inf = "Player: " + a ;
+            string inf2 = "SteamId: " + s64 ;
+            plugin.Debug(inf);
+            plugin.Debug(inf2);
             //plugin.Info("Here");
             //return new string[] { args.Length.ToString() };
             //string path = @"..\k.json";
             if (args != null && args.Length > 0)
             {
-                //Server.Write("stuff", ConsoleColor.Magenta);
                 //Dictionary<string, Dictionary<string, List<string>>> _data = new Dictionary<string, Dictionary<string, List<string>>>();
                 string x = args[0].ToLower();
-                string path = @"..\config.xml";
+                string path1 = @"rc-config-1.xml";
                 if (args.Length > 1)
                 {
-                    string cl = args[1];
+                    string cl = args[1].ToLower();
                     //Dictionary<string, List<string>> classitems = new Dictionary<string, List<string>>();
                     //classitems.Add(cl, items);
                     if (args != null && args.Length > 2) 
                     {
-                        //int len = args.Length - 2;
-                        //string[] items = new string[len];
+                        int len = args.Length - 2;
+                        //string[] itemlist = new string[len];
 
-                        XmlWriter xmlWriter = XmlWriter.Create(path);
+                        XmlDocument xmlDoc = new XmlDocument();
+                        XmlNode rootNode = xmlDoc.CreateElement("ranks");
+                        xmlDoc.AppendChild(rootNode);
 
-                        xmlWriter.WriteStartDocument();
-                        xmlWriter.WriteStartElement("ranks");
+                        XmlNode rank = xmlDoc.CreateElement(x);
+                        XmlAttribute cla = xmlDoc.CreateAttribute("class");
+                        cla.Value = cl;
+                        rank.Attributes.Append(cla);
 
-                        xmlWriter.WriteStartElement(x);
-                        xmlWriter.WriteStartElement(args[1]);
+                        XmlNode items = xmlDoc.CreateElement("items");
 
                         for (int i = 2; i < args.Length; i++)
                         {
-                            //items[i - 2] = args[i];
-                            xmlWriter.WriteString(args[i]);
+                            int itemint = i - 2;
+                            string itemname = "item" + itemint;
+                            XmlNode item = xmlDoc.CreateElement(itemname);
+                            item.InnerText = args[i];
                         }
 
-                        xmlWriter.WriteEndElement();
+                        xmlDoc.AppendChild(items);
+                        rootNode.AppendChild(rank);
 
-                        xmlWriter.WriteEndDocument();
-                        xmlWriter.Close();
+                        xmlDoc.Save(path1);
+                        //XmlWriter xmlWriter = XmlWriter.Create(path);
 
-                        //_data.Add(x, classitems);
+                        //xmlWriter.WriteStartDocument();
+                        //xmlWriter.WriteStartElement("ranks");
 
-                        //using (StreamWriter file = File.CreateText(path))
+                        //xmlWriter.WriteStartElement(x);
+                        //xmlWriter.WriteStartElement(args[1]);
+
+                        //for (int i = 2; i < args.Length; i++)
                         //{
-                        //    XmlSerializer ser = new XmlSerializer(typeof(Dictionary<string, Dictionary<string, List<string>>>));
-                        //    ser.Serialize(file, _data);
-                        //    //JsonSerializer serializer = new JsonSerializer();
-                        //    //serializer.Serialize(file, _data);
+                        //    //itemlist[i - 2] = args[i];
+                        //    //xmlWriter.WriteString(args[i]);
                         //}
-                        return new string[] { "Saved configuration" };
+
+                        //xmlWriter.WriteEndElement();
+
+                        //xmlWriter.WriteEndDocument();
+                        //xmlWriter.Close();
+
+                        return new string[] { "Saved configuration for " + x + ":" + cl };
                     }
                     else
                     {
@@ -98,11 +119,36 @@ namespace ExamplePlugin
             {
                 return new string[] { GetUsage() };
             }
-
         }
 
+        static public void Serialize()
+        {
+            
+        }
     }
 }
+
+//***** Welcome to the bottom of the file *****
+//**** Here are several things I wrote out to help me code: ****
+//
+//** Example Commands: **
 //save something something 15,2,26,1
 //save owner something 1,2,3
 //save laneklfhak sakfneoiqoia 928,129u48,127487
+//
+//** Example Xml: **
+//<ranks>
+//  <owner class="scientist">
+//      <items>
+//          <item1>SomeItem</item1>
+//          <item2>SomeOtherItem</item2>
+//      </items>
+//  </owner>
+//  <admin class="class-d">
+//      <items>
+//          <item1>SomeItem</item1>
+//          <item2>CouldBeAnyItem</item2>
+//          <item3>CanHaveAsManyAsYouWant</item3>
+//      </items>
+//  </admin>
+//</ranks>
