f09b0aa5b73c7217b2eafb7743b71a34c31a240a
Lord of Khaos <30538145+lordofkhaos@users.noreply.github.com>
2018-09-14 13:47:10 -0400

    Update Plugin.cs

--- RoleClass/Plugin.cs
+++ RoleClass/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using RoleClass;
+using RoleClass;
 using RoleClass.Commands;
 using Smod2;
 using Smod2.Attributes;
@@ -11,34 +11,34 @@ using System.Text;
 
 namespace RoleClass
 {
-	[PluginDetails(
-		author = "lordofkhaos",
-		name = "RoleClass",
-		description = "Give certain items to roles",
-		id = "com.lordofkhaos.roleclass",
-		version = "1.1",
-		SmodMajor = 3,
-		SmodMinor = 1,
-		SmodRevision = 16
-		)]
-	class RoleClass : Plugin
-	{
-		public override void OnDisable()
+    [PluginDetails(
+        author = "lordofkhaos",
+        name = "RoleClass",
+        description = "Give certain items to roles",
+        id = "com.lordofkhaos.roleclass",
+        version = "1.1",
+        SmodMajor = 3,
+        SmodMinor = 1,
+        SmodRevision = 16
+        )]
+    class RoleClass : Plugin
+    {
+        public override void OnDisable()
         {
             this.Info("Plugin disabled!");
         }
 
-		public override void OnEnable()
-		{
-			this.Info("RoleClass loaded successfully!");
-		}
+        public override void OnEnable()
+        {
+            this.Info("RoleClass loaded successfully!");
+        }
 
         private EventHandler events;
 
-		public override void Register()
-		{
+        public override void Register()
+        {
             events = new EventHandler(this);
-			// Register Events
+            // Register Events
             this.AddEventHandler(typeof(IEventHandlerPlayerJoin), events, Priority.Low);
             this.AddEventHandler(typeof(IEventHandlerSetRole), events, Priority.Highest);
             // Register Commands
@@ -46,8 +46,8 @@ namespace RoleClass
             //this.AddCommands(new string[] { "del", "rem", "rentry" }, new Commands.DeleteCommand());
             // Register config settings
             this.AddConfig(new Smod2.Config.ConfigSetting("k_enable", true, Smod2.Config.SettingType.BOOL, true, "Enable RoleClass"));
-            this.AddConfig(new Smod2.Config.ConfigSetting("k_global_give", new Dictionary<string, string>() {}, true, Smod2.Config.SettingType.DICTIONARY, true, "Roles and items"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("k_global_give", new Dictionary<string, string>() { }, true, Smod2.Config.SettingType.DICTIONARY, true, "Roles and items"));
             // tba
-		}
-	}
+        }
+    }
 }
