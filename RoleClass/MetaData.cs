5304a6dcbf17ca5c1f60f34169525f6e9f0f4191
lordofkhaos <liam@billandmary.com>
2019-01-25 22:04:51 -0500

    Major version bump & Smod version update

--- RoleClass/MetaData.cs
+++ RoleClass/MetaData.cs
@@ -6,13 +6,13 @@
 		internal const string Author = "lordofkhaos";
 		internal const string Description = "Give items to people who have the role and class";
 		internal const string ID = "com.lordofkhaos.roleclass";
-		internal const string MajorVersion = "1";
-		internal const string MinorVersion = "4";
-		internal const string Build = "3";
-		internal const string Revision = "B";
-		internal const string Version = MetaData.MajorVersion + "." +MetaData.MinorVersion + "." + Build + "-" + Revision;
+		internal const string MajorVersion = "2";
+		internal const string MinorVersion = "0";
+		internal const string Build = "0";
+		internal const string Revision = "A";
+		internal const string Version = MetaData.MajorVersion + "." + MetaData.MinorVersion + "." + Build + "-" + Revision;
 		internal const int SmodMajor = 3;
-		internal const int SmodMinor = 1;
-		internal const int SmodBuild = 22;
+		internal const int SmodMinor = 2;
+		internal const int SmodBuild = 2;
 	}
 }
