a58b1e32fb925cb6bf96e1f54a7872997d879859
lordofkhaos <liam@billandmary.com>
2019-01-30 20:02:59 -0500

    Fixed namespacing

--- RoleClass/Essential/MetaData.cs
+++ RoleClass/Essential/MetaData.cs
@@ -1,16 +1,16 @@
-﻿namespace RoleClass
+﻿namespace RoleClass.Essential
 {
 	internal struct MetaData
 	{
 		internal const string Name = "RoleClass";
 		internal const string Author = "lordofkhaos";
 		internal const string Description = "Do things specific to roles and classes";
-		internal const string ID = "com.lordofkhaos.roleclass";
+		internal const string Id = "com.lordofkhaos.roleclass";
 		internal const string MajorVersion = "2";
 		internal const string MinorVersion = "0";
 		internal const string Build = "0";
 		internal const string Revision = "A";
-		internal const string Version = MetaData.MajorVersion + "." + MetaData.MinorVersion + "." + Build + "-" + Revision;
+		internal const string Version = MajorVersion + "." + MinorVersion + "." + Build + "-" + Revision;
 		internal const int SmodMajor = 3;
 		internal const int SmodMinor = 2;
 		internal const int SmodBuild = 2;
