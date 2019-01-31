04150ce6cd97bded9e734030328cf65b11949852
lordofkhaos <liam@billandmary.com>
2019-01-31 12:39:14 -0500

    Fixed DeleteCommand

--- RoleClass/Commands/DeleteCommand.cs
+++ RoleClass/Commands/DeleteCommand.cs
@@ -6,6 +6,7 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
 using RoleClass;
+using RoleClass.Assists;
 using Smod2;
 using Smod2.API;
 using Smod2.Commands;
@@ -14,7 +15,7 @@ namespace RoleClass.Commands
 {
 	internal class DeleteCommand : ICommandHandler
 	{
-		private RoleClass _plugin;
+		private readonly RoleClass _plugin;
 		public DeleteCommand(RoleClass plugin)
 		{
 			_plugin = plugin;
@@ -22,7 +23,7 @@ namespace RoleClass.Commands
 
 		public string GetCommandDescription()
 		{
-			// This prints when someone types HELP SAVE
+			// This prints when someone types HELP DEL
 			return "Deletes a set of items for the role/class.";
 		}
 
@@ -33,62 +34,48 @@ namespace RoleClass.Commands
 
 		public string[] OnCall(ICommandSender sender, string[] args)
 		{
+			// Don't do anything if legacy mode is disabled
 			if (!_plugin.GetConfigBool("krc_legacy_enable")) return new string[] { };
-			//if (sender is Player pl)
-			//{
-			//    //Player a = (Player)sender;
-			//    var s64 = pl.SteamId;
-			//    string inf = "Player: " + pl;
-			//    string inf2 = "Steamid: " + s64;
-			//    if (!string.IsNullOrEmpty(pl.SteamId))
-			//    {
-			//        return new string[] { inf + inf2 };
-			//        //plugin.Debug(inf2);
-			//    }
-			//}
-			Dictionary<string, string> myDeserializedData = new Dictionary<string, string>();
-			if (args != null && args.Length > 0)
+			// Insufficient args
+			if (args == null || args.Length < 1) return new[] { GetUsage() };
+			if (args[0].ToLower().Compare(StringComparison.OrdinalIgnoreCase, "all", "every", "*"))
 			{
-				string trueRankName = args[0].ToLower();
-				// cleanup old files
-				string path = @"rc-config.dat";
-				string path2 = @"dictionary.bin";
-				if (File.Exists(path))
-				{
-					File.Delete(path);
-				}
-
-				if (File.Exists(path2))
-				{
-					File.Delete(path2);
-				}
-				//
-				if (args.Length > 1)
-				{
-					// parse class
-					string trueClassName = args[1].ToLower();
-					IFormatter formatter = new BinaryFormatter();
-					if (File.Exists("roleclass.cfgbin"))
-					{
-						using (FileStream fileStream = File.Open("roleclass.cfgbin", FileMode.OpenOrCreate))
-						{
-							myDeserializedData = (Dictionary<string, string>)formatter.Deserialize(fileStream);
-						}
-					}
-					foreach (KeyValuePair<string, string> pair in myDeserializedData)
-					{
-						string myClassName = pair.Value.Substring(trueClassName.Length);
-						if (pair.Key == trueRankName && pair.Value.StartsWith(myClassName, StringComparison.Ordinal))
-						{
-							pair.Equals(new Dictionary<string, string>() { });
-						}
-
-						return new string[] { "Removed configuration for " + trueRankName + ":" + trueClassName };
-					}
-				}
-				return new string[] { GetUsage() };
+				File.Delete(Ancillary.LegacyBinCfgPath);
+				return new[] { "File successfully deleted" };
 			}
-			return new string[] { GetUsage() };
+			// Parse the class and role from arguments
+			string trueRankName = args[0].ToLower();
+			string trueClassName = args[1].ToLower();
+			// If the file doesn't exist, return an error message
+			if (!File.Exists(Ancillary.LegacyBinCfgPath)) return new[] { "<color=#ff0000>File not found!</color>" };
+			Dictionary<string, string> deserialized;
+			// Open the file
+			using (FileStream fileStream = File.Open(Ancillary.LegacyBinCfgPath, FileMode.Open))
+			{
+				IFormatter formatter = new BinaryFormatter();
+				// Deserialize
+				deserialized = (Dictionary<string, string>)formatter.Deserialize(fileStream);
+			}
+			if (!deserialized.ContainsKey(trueRankName))
+				return new[] { $"Configuration for {trueRankName}::{trueClassName} not found" };
+			
+			// Loop through the object 
+			foreach (KeyValuePair<string, string> pair in deserialized)
+			{
+				if (pair.Key != trueRankName || !pair.Value.StartsWith(trueClassName))
+					continue;
+				// Remove it
+				deserialized.Remove(trueRankName);
+				File.Delete(Ancillary.LegacyBinCfgPath);
+				// Write the object to a new file
+				using (StreamWriter sw = new StreamWriter(Ancillary.LegacyBinCfgPath))
+				{
+					sw.Write(deserialized);
+				}
+				return new[] { $"Removed configuration for {trueRankName}::{trueClassName}" };
+			}
 
+			return new[] { GetUsage() };
 		}
 	}
+}
